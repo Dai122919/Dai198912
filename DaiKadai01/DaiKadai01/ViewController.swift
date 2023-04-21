07d510b110257a03a57adbc21aad310aa5ab2214
akio0911 <akio0911@gmail.com>
2023-04-21 23:16:39 +0900

    feedback

--- DaiKadai01/DaiKadai01/ViewController.swift
+++ DaiKadai01/DaiKadai01/ViewController.swift
@@ -8,26 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var number1: UITextField!
-    @IBOutlet weak var number2: UITextField!
-    @IBOutlet weak var number3: UITextField!
-    @IBOutlet weak var number4: UITextField!
-    @IBOutlet weak var number5: UITextField!
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var number1TextField: UITextField!
+    @IBOutlet weak var number2TextField: UITextField!
+    @IBOutlet weak var number3TextField: UITextField!
+    @IBOutlet weak var number4TextField: UITextField!
+    @IBOutlet weak var number5TextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
 
     @IBAction func button(_ sender: Any) {
-        let num1: Int = Int(number1.text!) ?? 0
-        let num2: Int = Int(number2.text!) ?? 0
-        let num3: Int = Int(number3.text!) ?? 0
-        let num4: Int = Int(number4.text!) ?? 0
-        let num5: Int = Int(number5.text!) ?? 0
+        let num1: Int = Int(number1TextField.text!) ?? 0
+        let num2: Int = Int(number2TextField.text!) ?? 0
+        let num3: Int = Int(number3TextField.text!) ?? 0
+        let num4: Int = Int(number4TextField.text!) ?? 0
+        let num5: Int = Int(number5TextField.text!) ?? 0
 
-        label.text = String(num1 + num2 + num3 + num4 + num5)
+        resultLabel.text = String(num1 + num2 + num3 + num4 + num5)
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
 }
